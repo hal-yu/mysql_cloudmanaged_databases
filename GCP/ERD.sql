3bf11d052186c404f3e7bceae5b5555942f2191d
Haley Yu <124009232+hal-yu@users.noreply.github.com>
2023-10-01 19:48:28 -0400

    Update ERD.sql

--- GCP/ERD.sql
+++ GCP/ERD.sql
@@ -1,1 +1,36 @@
+CREATE TABLE patients (
+    patient_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE,
+    contact_number VARCHAR(20),
+	primary_doctor_id INT,
+    FOREIGN KEY (primary_doctor_id) REFERENCES doctors(doctor_id)
+);
 
+CREATE TABLE doctors (
+    doctor_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    specialization VARCHAR(50),
+    contact_number VARCHAR(20)
+);
+
+CREATE TABLE visits (
+    visit_id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT,
+    doctor_id INT,
+    visit_date DATETIME,
+    purpose VARCHAR(150),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id)
+);
+
+CREATE TABLE appointments (
+	appointment_id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT,
+    doctor_id INT,
+    appointment_date DATE NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id)
+);
